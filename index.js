98e70769b5bea3c2854c4200059af36ca43bd684
Welljhoon <jhon-43769@outlook.com>
2024-11-01 18:40:25 -0400

    Add user retrieval and update endpoints

--- index.js
+++ index.js
@@ -26,6 +26,31 @@ app.get('/api/users', async (req, res) => {
     }
 })
 
+app.get('/api/user/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const user = await User.findById(id)
+        res.status(200).json(user)
+    } catch (error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+app.put('/api/user/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const user = await User.findByIdAndUpdate(id, req.body)
+            if(!user) {
+                return req.status(404).json({message: "This product is not found"});
+            }   
+            const updateUser = await User.findById(id)
+            res.status(200).json(updateUser)
+    }catch (error){
+        res.status(500).json({message: error.message})
+    }
+})
+
 mongoose.connect("mongodb+srv://jhon437699:9jWGuABKtpzlNZAz@taskhub.prohz.mongodb.net/?retryWrites=true&w=majority&appName=TaskHub")
 .then(() => {
     console.log("connected to the dataBase")
